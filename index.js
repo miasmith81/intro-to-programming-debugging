f1e80124702cfbe48ab6ca3c1b1ca2c345cf86dc
miasmith81 <mismitha326@gmail.com>
2025-10-14 20:25:17 -0600

    Fix pluralization logic for remaining attempts in guess feedback

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ function checkGuess() {
     // Use singular/plural form based on remaining attempts
     // Stretch Goal: If there is only one guess left, it should say "guess" (singular) instead of "guesses" (plural)
     // BUG FIX #10: Adjusted pluralization logic to correctly handle 0 too 
-    const guessText = remainingAttempts < 2 ? 'guess' : 'guesses';
+    const guessText = remainingAttempts === 1 ? 'guess' : 'guesses';
     numberOfGuessesMessage.innerHTML = `You guessed ${guess}. <br> ${remainingAttempts} ${guessText} remaining`;
   }
 
